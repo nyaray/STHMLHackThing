6a6a56fb4bae8d82f1f8f65b3bf27e6e0ef96d80
Christopher Loessl <cloessl@x-berg.de>
2013-05-09 17:01:02 +0200

    You can get the intersection of two persons likes
    $ ./get.sh me "likes.fields(name)" | tr -d " " | grep '"name":' > me
    + for another person
    $ comm -12 <(sort me) <(sort otherPerson)

--- get.sh
+++ get.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# put your secret token in there
-source token.txt
 
-# "me" referes to the actuall user
-USER=me
+# the config file, a template can be found at config.sample.txt
+source config.txt
 
-# fields you want to get back
-#FIELDS=id,name,picture # this works w/o token but you have to provide a USER (not "me")
-FIELDS=id,name,likes
+# you can override the user and fields variable from the command line
+# $ ./get.sh <user> <fields>
+USER=$1
+FIELDS=$2
 
 # build the request together and beautify it
 #wget -qO - https://graph.facebook.com/$USER?fields=$FIELDS\&access_token=$TOKEN | cat | python -mjson.tool
-curl https://graph.facebook.com/$USER?fields=$FIELDS\&access_token=$TOKEN | python -mjson.tool
+echo "curl https://graph.facebook.com/$USER?fields=$FIELDS&access_token=\$TOKEN | python -mjson.tool"
+curl "https://graph.facebook.com/$USER?fields=$FIELDS&access_token=$TOKEN" | python -mjson.tool
 
 ## side note
 ## sh get.sh | grep category | sort | uniq
