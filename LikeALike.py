f12a6ce586de6edd859172d88280c73fe9636fad
Christopher Loessl <cloessl@x-berg.de>
2013-05-11 14:56:00 +0200

    got a dict with all intersection with names and ids

--- LikeALike.py
+++ LikeALike.py
@@ -7,7 +7,7 @@ from pprint import pprint
 
 # put the facebook auth token here
 # later get it directly through the JS SDK provided from FB
-oauth_access_token = "CAACEdEose0cBAEKhZCdUZAap7Vpnt91ZAEpXEBKHj1QVJF1cuxCnSUf8qedRKsP6pPWc9uBVv6lyw36XcQOkLqlgu0PagZCZBAbssFVhZA5hmBswCmXmk3yOJfgZCT47yInwew2Mun9rFZAKqvetGaCFzo5mzDalHbZC3nVwHL0J73gZDZD"
+oauth_access_token = "CAACEdEose0cBAEh7ZCbvNIeN7qPihDQE6mn1PwGEAAUzXPyzUQhk61oA8Dw50LBGMSCAgRWwP4H7RYlnDu2Tbk4DZAQ9CliZB1oU2YP1V26xUQLZCgqYnzio7F8uoPjTb1tseslZCcxCEyY0M9rPXLHZBOux8xV766g4UGkPpufQZDZD"
 
 
 def getGraph():
@@ -26,12 +26,17 @@ def getFriends(graph, profile):
     return graph.get_connections(profile['id'], "friends")
 
 
-def getIDs(likes):
+def getIDList(likes):
     tmpList = []
     for element in likes['data']:
-        # print u"{}".format(element['name']).encode(sys.getfilesystemencoding())
+        # print
+        # u"{}".format(element['name']).encode(sys.getfilesystemencoding())
         tmpList.append(int(element['id']))
     return tmpList
+
+
+def getIntersection(listA, listB):
+    return list(set(listA) & set(listB))
 
 
 def main(argv):
@@ -40,14 +45,19 @@ def main(argv):
     myProfile = getProfile(graph)
     meLikes = getLikes(graph, myProfile)
 
-    chrisProfile = getProfile(graph, "christopher.loessl")
-    chrisLikes = getLikes(graph, chrisProfile)
+    # chrisProfile = getProfile(graph, "christopher.loessl")
+    # chrisLikes = getLikes(graph, chrisProfile)
     nyarayProfile = getProfile(graph, "nyaray")
     nyarayLikes = getLikes(graph, nyarayProfile)
     miaProfile = getProfile(graph, "mia5419")
     miaLikes = getLikes(graph, miaProfile)
     jensProfile = getProfile(graph, "jens.rosen")
     jensLikes = getLikes(graph, jensProfile)
+
+    # friends = {'nyarayProfile['id']': nyarayLikes,
+    #            'miaProfile['id']': miaLikes,
+    #            'jensProfile['id']': jensLikes
+    #            }
 
     # gives me the data structure to work with
     # DEBUG!!
@@ -58,13 +68,33 @@ def main(argv):
     # this is a list of dictionaries
     # this will be needed to have a mapping between id -> name
     listOfCoolStuff = meLikes['data']
+    meLikesIDList = getIDList(meLikes)
 
-    listA = getIDs(meLikes)
-    listB = getIDs(miaLikes)
-    setA = set(listA)
-    setB = set(listB)
-    setInter = setA & setB
-    print setInter
+    friendsList = []
+    friendsList.extend(getIDList(miaLikes))
+    friendsList.extend(getIDList(jensLikes))
+    friendsList.extend(getIDList(nyarayLikes))
+
+    intersectionList = []
+    intersectionList.extend(getIntersection(getIDList(meLikes), getIDList(miaLikes)))
+    intersectionList.extend(getIntersection(getIDList(meLikes), getIDList(jensLikes)))
+    intersectionList.extend(getIntersection(getIDList(meLikes), getIDList(nyarayLikes)))
+
+    stuffInCommonDict = {}
+    finalList = []
+    for element in meLikes['data']:
+        # print element['id']
+        if (int(element['id']) in intersectionList):
+            finalList.append(element['name'])
+            stuffInCommonDict[element['name']] = int(element['id'])
+            stuffInCommonDict[element['id']] = (element['name'])
+        # print
+        # u"{}".format(element['name']).encode(sys.getfilesystemencoding())
+        # tmpList.append(int(element['id']))
+    finalList.sort()
+    # pprint(finalList)
+    # print "-------------"
+    pprint(stuffInCommonDict)
 
 
 if __name__ == '__main__':
